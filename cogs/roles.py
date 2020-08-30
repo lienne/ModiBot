b3404fac57367135d9e95d51b2d0edc8aea8c788
lienne <lianasevero3@gmail.com>
2020-08-30 19:46:23 -0400

    Added vibe check command.

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MSGID = os.getenv('TRUSTED_ROLES_MSG_ID')
 
 class Roles(commands.Cog):
 
@@ -9,18 +14,47 @@ class Roles(commands.Cog):
     @bot.event
     async def on_raw_reaction_add(payload):
         message_id = payload.message_id
-        if message_id == 749716013196509264:
+        if message_id == MSGID:
             guild_id = payload.guild_id
             guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
 
             if payload.emoji.name == 'rooDevil':
-                print('manager')
-            # role = discord.utils.get(guild.roles, name = )
+                role = discord.utils.get(guild.roles, name = 'manager')
+            elif payload.emoji.name == 'smugcat4':
+                role = discord.utils.get(guild.roles, name = 'trans')
+            else:
+                role = discord.utils.get(guild.roles, name = payload.emoji.name)
+            
+            if role is not None:
+                member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+                if member is not None:
+                    await member.add_roles(role)
+                    print('Done.')
+            else:
+                print('Role not found.')
 
 
     @bot.event
     async def on_raw_reaction_remove(payload):
-        pass
+        message_id = payload.message_id
+        if message_id == MSGID:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g : g.id == guild_id, bot.guilds)
+
+            if payload.emoji.name == 'rooDevil':
+                role = discord.utils.get(guild.roles, name = 'manager')
+            elif payload.emoji.name == 'smugcat4':
+                role = discord.utils.get(guild.roles, name = 'trans')
+            else:
+                role = discord.utils.get(guild.roles, name = payload.emoji.name)
+            
+            if role is not None:
+                member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+                if member is not None:
+                    await member.remove_roles(role)
+                    print('Done.')
+            else:
+                print('Role not found.')
 
 # Connect cog to bot
 def setup(bot):
---
+++ cogs/vibes.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+import asyncio
+import hashlib
+import random
+
+class Vibes(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command('vibe', desc=['Perform a vibe check.', '- Check your own vibe', '@user - Check <user>\'s vibe'],
+            min = 0, max = float('inf'), category = 'Fun')
+    async def vibe(self, ctx, target : discord.Member = None):
+
+        vibes = [':crown: Top', ':peach: Bottom', ':full_moon_with_face: Strange', ':rose: Charm', ':boxing_glove: Chutzpah',
+            ':coffee: Cozy', ':skull: Cursed', ':chocolate_bar: Ravenous']
+
+        if target is None:
+            target = ctx.author
+
+        resp = await ctx.send('🔮 | Checking **{}**\'s vibe...'.format(target))
+        await asyncio.sleep(5.0)
+
+        h = hashlib.sha256()
+        h.update(target.id.to_bytes(8, 'big'))
+
+        values = [x % 100 for x in list(h.digest())[7:15]]
+
+        for i, v in enumerate(values):
+            values[i] = int(max(min(v + random.gauss(0, 10), 100), 0))
+
+        if values[4] > 66 or any(v > 80 for v in values) or values[3] > 60 or values[6] < 20:
+            desc = ':white_check_mark: **| Vibe check passed**'
+
+        else:
+            desc = ':no_entry_sign: **| Vibe check failed**'
+
+        embed = discord.Embed(title='Vibe Check', description = desc, color = 0x7ce4f7)
+        embed.set_author(name = str(target), icon_url = target.avatar_url)
+        embed.set_footer(text = 'Checked by {}'.format(ctx.author), icon_url = ctx.author.avatar_url)
+
+        for vibe, value in zip(vibes, values):
+            bars = int((value/100.0)*10)
+            embed.add_field(name='**{} | {}% |**'.format(vibe, value), inline=False,
+                            value='|' + '▬'*bars + ':radio_button:' + '▬'*(10-bars) + '|')
+
+        await resp.edit(content='', embed = embed)
+
+# Connect cog to bot
+def setup(bot):
+    bot.add_cog(Vibes(bot))
