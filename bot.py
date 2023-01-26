aa0126dd26b6ffcdc043b6049ebfbc9d6f30415c
belle <lianasevero3@gmail.com>
2023-01-25 20:29:16 -0800

    Updated main bot file, added a tic tac toe game command

--- bot.py
+++ bot.py
@@ -1,75 +1,67 @@
 import os
 import logging
-import random
 import discord
 import asyncio
-import builtins
 
 from dotenv import load_dotenv
 from discord.ext import commands
-from typing import Literal, Optional
-from discord.ext.commands import Greedy, Context
 
-intents = discord.Intents.default()
-intents.presences = True
-intents.members = True
-intents.message_content = True
+class VerifyButton(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        # self.value = None
 
-bot = commands.Bot(command_prefix = '!', activity = discord.Game('with the discord API!'), intents=intents)
-bot.remove_command('help')
-builtins.bot = bot
+    @discord.ui.button(label='Verify', style=discord.ButtonStyle.green, custom_id='verify_button:green')
+    async def green(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message('Private thread created.', ephemeral=True)
+        # self.value = True
+        # self.stop()
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
-discord.utils.setup_logging(level=logging.INFO, root=False)
+class MyBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.presences = True
+        intents.members = True
+        intents.message_content = True
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.ERROR)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
-logger.addHandler(handler)
+        super().__init__(
+            command_prefix = commands.when_mentioned_or('!'),
+            help_command=None,
+            activity = discord.Game('with the discord API!'),
+            intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has joined the chat!')
+    discord.utils.setup_logging(level=logging.INFO, root=False)
+
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.ERROR)
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+    logger.addHandler(handler)
+
+    async def setup_hook(self) -> None:
+        self.add_view(VerifyButton())
+
+    async def on_ready(self):
+        print(f'{self.user.name} has joined the chat!')
+        print('----------')
+
+bot = MyBot()
 
 @bot.command()
-@commands.guild_only()
 @commands.is_owner()
-async def sync(
-    ctx: Context, guilds: Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
-        if not guilds:
-            if spec == "~":
-                synced = await ctx.bot.tree.sync(guild=ctx.guild)
-            elif spec == "*":
-                ctx.bot.tree.copy_global_to(guild=ctx.guild)
-                synced = await ctx.bot.tree.sync(guild=ctx.guild)
-            elif spec == "^":
-                ctx.bot.tree.clear_commands(guild=ctx.guild)
-                await ctx.bot.tree.sync(guild=ctx.guild)
-                synced = []
-            else:
-                synced = await ctx.bot.tree.sync()
-
-            await ctx.send(
-                f"Synced {len(synced)} commands {'globally.' if spec is None else 'to the current guild.'}"
-            )
-            return
-
-        ret = 0
-        for guild in guilds:
-            try:
-                await ctx.bot.tree.sync(guild=guild)
-            except discord.HTTPException:
-                pass
-            else:
-                ret += 1
-
-        await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+async def prepare(ctx: commands.Context):
+    """Starts a persistent view."""
+    # In order for a persistent view to be listened to, it needs to be sent to an actual message.
+    # Call this method once just to store it somewhere.
+    await ctx.send("Click here to create a new private thread.", view=VerifyButton())
 
 async def main():
+
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    # GUILD = os.getenv('DISCORD_GUILD')
+
     @bot.command()
     async def load(ctx, extension):
         await bot.load_extension(f'cogs.{extension}')
